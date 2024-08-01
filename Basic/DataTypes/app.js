8a43cc20ed9137803ea6fb0588508e4891a10034
Sachin Sharma <13688737+sharmasachin013@users.noreply.github.com>
2024-08-01 12:56:14 +0000

    Data types notes

---
+++ Basic/DataTypes/app.js
@@ -0,0 +1,40 @@
+//Primitive data types
+
+//Boolean : Boolean represents a logical entity and can have two values: true, and false.
+
+//Number : Represents numeric values e.g. 100
+
+//String : Represents sequence of characters e.g. "hello"
+
+//Null : Represents null i.e. no value at all
+
+//Undefined : Represents undefined value
+
+// Non  Primitive data types
+//Object : Represents instance through which we can access members
+
+//Array : Represents group of similar values
+// Boolean Data Type
+// Booleans can only have two values: true or false.
+// Example let x =true;
+// Number Data Type
+// Numbers in JavaScript are just numbers , no integers and no floats , just plain numbers
+// salary=50000; pi=3.14
+
+// String Data Type
+// => A String is just a combination of words including nos.
+// => It is enclosed in  a pair of “    “  or   ‘     ‘ .
+// Example let city = "Indore" Or let city='Indore'
+
+// ==============================
+// undefined And null
+// null and undefined are both data types in JavaScript.
+// What Is undefined ?
+// It means a variable has been declared, but no value has been assigned to it.
+// In simple words we can say that , JavaScript automatically assigns undefined to any variable which is declared but not initialized
+// What Is null ?
+// It is a value that is assigned by programmer, to indicate that the variable currently has no value but will be initialized later
+// In simple words we can say that , null represents intentional absence of the value
+// Example
+//var age=null  ;         //  age is declared and initialized with null
+//console.log(age);  //  Will show null
---
+++ Basic/DataTypes/readme.md
@@ -0,0 +1,11 @@
+Boolean : Boolean represents a logical entity and can have two values: true, and false.
+
+Number : Represents numeric values e.g. 100
+
+String : Represents sequence of characters e.g. "hello"
+
+Null : Represents null i.e. no value at all
+
+Undefined : Represents undefined va
+
+
