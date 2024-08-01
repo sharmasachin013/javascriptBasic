05eb127e9c2f30c1cb8b2a1f76b9ab7f48b95913
Sachin Sharma <13688737+sharmasachin013@users.noreply.github.com>
2024-08-01 04:27:12 +0000

    variables example

---
+++ Basic/VARIABLES/app.js
@@ -0,0 +1,25 @@
+//Declaring Variables
+
+//JavaScript is a weakly typed programming language.
+// Rather JavaScript allows us to store any value in a variable.
+// var,let, const
+// var  <var name>;
+// var <var name>=<value>; name="sachin";
+// var firstName;
+// var firstName, lastName;
+// var firstName="sachin"
+
+//Variables Declaration Rules
+// Variable names must begin with a letter and can have digits also
+// Variable names can also begin with $ and _
+// Variable names are case sensitive
+// Default value of a variable is “undefined”
+// var firstName
+let firstName = "sachin";
+const lastname = "anil";
+let x = 50;
+//lastname = "amit";
+console.log(lastname);
+//let firstName = "anil";
+//console.log(firstName);
+console.log(typeof x);
