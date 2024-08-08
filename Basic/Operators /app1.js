35590835e39da52d235a2fcfac47e9809887abae
Sachin Sharma <13688737+sharmasachin013@users.noreply.github.com>
2024-08-08 10:34:41 +0000

    Comparison Operators Example

---
+++ Basic/Operators /app1.js
@@ -0,0 +1,9 @@
+console.log("Equal (5 == '5'):", 5 == "5"); // true
+console.log("Strict Equal (5 === '5'):", 5 === "5"); // false
+console.log(5 != 5);
+console.log("Not Equal (5 != '5'):", 5 != "5"); // false
+console.log("Strict Not Equal (5 !== '5'):", 5 !== "5"); // true
+console.log("Greater Than (10 > 5):", 10 > 5); // true
+console.log("Greater Than or Equal (10 >= 10):", 10 >= 10); // true
+console.log("Less Than (3 < 7):", 3 < 7); // true
+console.log("Less Than or Equal (5 <= 5):", 5 <= 5); // true
