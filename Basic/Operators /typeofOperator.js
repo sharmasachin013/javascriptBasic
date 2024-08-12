623d44a30128745d4972327509c55f261f821088
Sachin Sharma <13688737+sharmasachin013@users.noreply.github.com>
2024-08-12 05:10:53 +0000

    typeof example code

---
+++ Basic/Operators /typeofOperator.js
@@ -0,0 +1,8 @@
+console.log(typeof 42);
+// output: "number“
+console.log(typeof ‘bhopal');
+// output: "string“
+console.log(typeof true);
+// output: "boolean“ 
+console.log(typeof (3+4));
+// output: “number"
