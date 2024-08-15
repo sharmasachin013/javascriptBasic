4b38c8db9a58c0542a8a9c0e1ebddf42d343ea2c
Sachin Sharma <13688737+sharmasachin013@users.noreply.github.com>
2024-08-15 10:02:51 +0000

    TypeConversion Example

---
+++ Basic/TypeConversion/app.js
@@ -0,0 +1,32 @@
+console.log(String(10));
+//"10"
+
+(10).toString();
+//"10"
+
+String(true);
+//"true"
+
+true.toString();
+//"true"
+
+String(false);
+//"false"
+
+false.toString();
+//"false”
+
+String(null);
+//"null"
+
+String(undefined);
+//"undefined“
+
+String(NaN);
+//"NaN”
+
+NaN.toString();
+//"NaN”
+// the method toString()
+// doesn’t work with null and undefined types .
+// Although it works with NaN
