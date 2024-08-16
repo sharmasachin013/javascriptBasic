10cda480d0c581ee5a2eb14deb6954b000d8c2ba
Sachin Sharma <13688737+sharmasachin013@users.noreply.github.com>
2024-08-16 04:50:29 +0000

    updated

--- Basic/TypeConversion/app.js
+++ Basic/TypeConversion/app.js
@@ -2,30 +2,39 @@ console.log(String(10));
 //"10"
 
 (10).toString();
+console.log((12).toString());
 //"10"
 
 String(true);
+console.log(String(true));
 //"true"
 
 true.toString();
+console.log(true.toString());
 //"true"
 
 String(false);
+console.log(false.toString());
 //"false"
 
 false.toString();
+console.log(false.toString());
 //"false”
 
 String(null);
+console.log(String(null));
 //"null"
 
 String(undefined);
+console.log(String(undefined));
 //"undefined“
 
 String(NaN);
+console.log(String(NaN));
 //"NaN”
 
 NaN.toString();
+console.log(typeof NaN.toString());
 //"NaN”
 // the method toString()
 // doesn’t work with null and undefined types .
