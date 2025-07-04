13523ff90d705e027824578bf7cb74a205bdc8d4
Sachin Sharma <sharma.sachin013@gmail.com>
2025-07-04 12:12:50 +0000

    updated js code

---
+++ Basic/TypeConversion/app1.js
@@ -0,0 +1,17 @@
+// Converting Values To Number
+// 1.Using the global function called Number( )
+// 2. Using the function parseInt( ) or parseFloat( )
+// 3. Using the method Math.floor( )
+// number(value);
+// let n = Number(undefined);
+// console.log(n); // 0
+
+let n = Math.floor("");
+let x = Number(null);
+console.log(n);
+console.log(x);
+
+// undefined NAN
+// null 0
+// true and false 1 and 0
+// Stifng NAN
