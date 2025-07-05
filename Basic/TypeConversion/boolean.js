ecab006c971270ee6d20b1a5e829b8bf29548dd0
Sachin Sharma <sharma.sachin013@gmail.com>
2025-07-05 10:06:41 +0000

    boolean example added

--- Basic/TypeConversion/boolean.js
+++ Basic/TypeConversion/boolean.js
@@ -5,8 +5,21 @@
 // IMPORTANT POINTS:
 
 // 1. Any value can be converted to boolean passing it to Boolean().
-
 // 2. All values will resolve to true except FALSY VALUES, which are
 // 0,””,NaN,null and undefined
-let n = Boolean();
+let n = Boolean(25);
+console.log(n);
+n = Boolean(0);
+console.log(n);
+n = Boolean("Sachin");
+console.log(n);
+n = Boolean("");
+console.log(n);
+n = Boolean(NaN);
+console.log(n);
+n = Boolean(null);
+console.log(n);
+n = Boolean(" ");
+console.log(n);
+n = Boolean(undefined);
 console.log(n);
