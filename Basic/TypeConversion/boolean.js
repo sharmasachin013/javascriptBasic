9bf7f4eb988034caea40cd04fea92982b87acd8f
Sachin Sharma <sharma.sachin013@gmail.com>
2025-07-05 00:03:48 +0000

    boolean example added

---
+++ Basic/TypeConversion/boolean.js
@@ -0,0 +1,9 @@
+// To convert a value to a number , JS provides only 1 way:
+
+// 1. Using the global function called Boolean( )
+// IMPORTANT POINTS:
+
+// 1. Any value can be converted to boolean passing it to Boolean().
+
+// 2. All values will resolve to true except FALSY VALUES, which are
+// 0,””,NaN,null and undefined
