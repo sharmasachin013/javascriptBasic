86f6c3d56fad0fb65b4cb5aa4f5a33a65bf4635d
Sachin Sharma <sharma.sachin013@gmail.com>
2025-07-05 14:37:49 +0000

    file added

---
+++ TypeCoercion/app.js
@@ -0,0 +1,19 @@
+// Type Coercion
+//there are some cases in JS , when it automatically converts value of one type
+// to another and this  is called Type Coercion
+console.log(10 + 20);
+console.log("Good" + "Morning");
+console.log("50" + 0);
+console.log(true + false);
+console.log(true + true);
+console.log(true + true + "");
+// The Behaviour of Other Arithmetic Operators:
+console.log(10 * 30);
+console.log("Good" * "Morning");
+console.log("50" * 2);
+console.log("50" - 25);
+console.log(true * true);
+console.log(true * false);
+console.log(5 * 2 * false);
+let x = 35.555555555;
+console.log(x.toFixed(2));
