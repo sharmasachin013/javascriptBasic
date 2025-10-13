21e226569c862a36c6be69cfdf9acb14d63e80c5
Sachin Sharma <sharma.sachin013@gmail.com>
2025-10-13 09:47:03 +0000

    code updated

---
+++ array-methods/app4.js
@@ -0,0 +1,15 @@
+// // / array.includes(element, startIndex)
+// const fruits = ["apple", "mango", "banana"];
+// console.log(fruits.includes("banana")); // true
+// console.log(fruits.includes("grape")); // false
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.includes(3, 0)); // true (starts at index 0)
+// console.log(numbers.includes(3, 3)); // true (starts at index 0)
+
+const fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits.includes("apple")); // false
+// == ,===
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 3, 10];
+console.log(numbers.includes(3, 0)); // true (starts at index 0)
