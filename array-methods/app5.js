154adaa686f69ff14f09576aa00517117f2df68b
Sachin Sharma <sharma.sachin013@gmail.com>
2025-10-18 06:37:45 +0000

    code updated

---
+++ array-methods/app5.js
@@ -0,0 +1,9 @@
+// array.indexOf(searchElement, fromIndex)
+const fruits = ["apple", "banana", "mango", "banana", "orange"];
+console.log(fruits.indexOf("Banana")); // Output: 1
+console.log(fruits.indexOf("orange")); // Output: 4
+console.log(fruits.indexOf("grape")); // Output: -1
+let text = "javascript is amazing!";
+
+let index = text.indexOf("Is");
+console.log(index); // output 11
