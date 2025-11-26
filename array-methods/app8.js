a23d570b8d916f45d977d480352ddd53fd7f4192
Sachin Sharma <sharma.sachin013@gmail.com>
2025-11-26 11:37:22 +0000

    code updated

---
+++ array-methods/app8.js
@@ -0,0 +1,7 @@
+let numbers = [10, 20, 30, 40, 50];
+// Display BEFORE reverse
+console.log("Before Reverse:", numbers);
+// Reverse the array
+numbers.reverse();
+// Display AFTER reverse
+console.log("After Reverse:", numbers);
---
+++ array-methods/app9.js
@@ -0,0 +1,4 @@
+// join() Method --> arry to string
+let fruits = ["Apple", "Banana", "Mango"];
+let str = fruits.toString();
+console.log(str);
