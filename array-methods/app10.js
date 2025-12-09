e5ddd2c23359f97096f624a2396ac0d97c8a75e3
Sachin Sharma <sharma.sachin013@gmail.com>
2025-12-09 05:44:31 +0000

    array method code updated

---
+++ array-methods/app10.js
@@ -0,0 +1,6 @@
+//at()
+const numbers = [10, 20, 30, 40, 50];
+console.log(numbers.at(-1));
+console.log(numbers.at(-2));
+console.log(numbers.at(-3));
+
---
+++ array-methods/app11.js
@@ -0,0 +1,4 @@
+let rahul = ["C", "Java", "Oracle"];
+let sumit = rahul.with(1, "JavaScript");
+console.log(rahul);
+console.log(sumit);
